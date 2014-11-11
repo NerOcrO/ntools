f6ecf03ab7d273db006875bf93a922f5370c938f
NerOcrO <nerocro@gmail.com>
2014-11-11 17:47:49 +0100

    bookmarklet added on README.md

--- ntools.user.js
+++ ntools.user.js
@@ -4,7 +4,7 @@
 // @description  Script who help developers on Drupal 7
 // @grant        none
 // @include      localhost
-// @version      1.0
+// @version      2.0
 // ==/UserScript==
 
 String.prototype.capitalize = function () {
